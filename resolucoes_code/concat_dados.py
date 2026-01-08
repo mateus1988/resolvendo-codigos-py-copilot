35f2e3e0532a61fcda598eee9e01d903b218b61e
Mateus da Silva Salomão <107901784+mateus1988@users.noreply.github.com>
2026-01-08 08:23:27 -0300

    Add user input and string concatenation functionality

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,9 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+
+
+palavra = input("Digite  algo: ")
+palavra2 = input("Digite o segundo dado: ")
+
+concatenacao = palavra + " " + palavra2
+print("Dados concatenados:", concatenacao)
