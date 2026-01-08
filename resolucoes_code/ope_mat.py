3a5bdd5fbde5115de7bb023fb11fca0ff1e54304
Mateus da Silva Salomão <107901784+mateus1988@users.noreply.github.com>
2026-01-08 08:24:06 -0300

    Implement basic arithmetic operations with user input

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,19 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+
+# solicitar entrada dois numeros e depois vamos reralizar as quatro operacoes matematicas basicas
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+# dar a opção para o usuario escolher a operação
+operacao = input("Escolha a operação (+, -, *, /): ")
+if operacao == '+':
+    print(num1 + num2)
+elif operacao == '-':
+    print(num1 - num2)
+elif operacao == '*':
+    print(num1 * num2)
+elif operacao == '/':
+    if num2 != 0:
+        print(num1 / num2)
+    else:
+        print("Erro: Divisão por zero não é permitida.")
